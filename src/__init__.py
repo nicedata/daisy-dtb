26129cdb1ddb762ae5873b4cd38bdf3d382c2f14
nicedata <m.mohnhaupt@bluewin.ch>
2025-01-13 23:20:04 +0100

    Cleanup

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.7"
 
-from daisy import Clip, MetaData, NccEntry, Par, Smil, Text
+from daisy import MetaData, NccEntry, Text
 from domlib import DomFactory
 from dtbsource import ZipDtbResource, FolderDtbResource
 
-__all__ = ["MetaData", "NccEntry", "Text", "Clip", "Par", "Smil", "DomFactory", "FolderDtbResource", "ZipDtbResource"]
+__all__ = ["MetaData", "NccEntry", "Text", "DomFactory", "FolderDtbResource", "ZipDtbResource"]
