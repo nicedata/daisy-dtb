f415fbf6853b549874f9af4da707ecfbb115cd76
nicedata <m.mohnhaupt@bluewin.ch>
2025-02-03 13:50:37 +0100

    More documentation added to DaisyBook

--- src/daisybook.py
+++ src/daisybook.py
@@ -1,4 +1,4 @@
-"""Daisy library"""
+"""Daisy Book related classes"""
 
 from dataclasses import dataclass, field
 from typing import List, Union
@@ -30,6 +30,18 @@ class DaisyBook:
     _smils: List[Smil] = field(init=False, default_factory=list)
 
     def __post_init__(self):
+        """DaisyBook instance post-initialization.
+
+        The steps are :
+        - Load the NCC.html file, then
+            - Extract the TOC entries
+            - Extact the metadata
+            - Create the SMIL entries
+            - Set the books title and the navigation depth
+
+        Raises:
+            DaisyBookException: this exception is raised when the instance cannot be set up.
+        """
         # Get the ncc.html file content
         ncc_document = self.source.get("ncc.html")
 
@@ -59,55 +71,122 @@ class DaisyBook:
 
     @property
     def cache_stats(self) -> dict:
+        """Get the cache statistics.
+
+        Returns:
+            dict: the cache statistics.
+        """
         return self.source._cache.get_stats()
 
     @property
     def title(self) -> str:
+        """Get the books title.
+
+        Returns:
+            str: the books title.
+        """
         return self._title
 
     @property
     def navigation_depth(self) -> int:
+        """Get the books navigation depth.
+
+        Returns:
+            int: the navigation depth.
+        """
         return self._navigation_depth
 
     @property
-    def metadata(self) -> int:
+    def metadata(self) -> List[MetaData]:
+        """Get the books metadata.
+
+        Returns:
+            List[MetaData]: the books metadata.
+        """
         return self._metadata
 
     @property
-    def smils(self) -> int:
-        return self.smils
+    def smils(self) -> List[Smil]:
+        """Get the books SMILs.
+
+        Returns:
+            List[Smil]: the books SMILs.
+        """
+        return self._smils
 
     @property
     def toc_entries(self) -> List[TocEntry]:
+        """Return all TOC entries as a list.
+
+        Returns:
+            List[TocEntry]: the TOC entries,
+        """
         return self._toc_entries
 
     @property
     def dc_metadata(self) -> List[MetaData]:
+        """Get the Dublin Core metadata.
+
+        Returns:
+            List[MetaData]: the Dublin Core metadata.
+        """
         return [_ for _ in self._metadata if _.name.startswith("dc:") and _.content is not None]
 
     @property
     def ncc_metadata(self) -> List[MetaData]:
+        """Get the Navigation Control Center metadata.
+
+        Returns:
+            List[MetaData]: the NCC metadata.
+        """
         return [_ for _ in self._metadata if _.name.startswith("ncc:") and _.content is not None]
 
     @property
     def other_metadata(self) -> List[MetaData]:
+        """Get the other metadata (not DC nor NCC).
+
+        Returns:
+            List[MetaData]: the other metadata.
+        """
         return [_ for _ in self._metadata if not _.name.startswith("dc:") and not _.name.startswith("ncc:") and _.content is not None]
 
     @property
     def langage(self) -> str:
+        """Get the book langage.
+
+        Returns:
+            str: the langage.
+        """
         metadata = self.get_metadata("dc:language")
         return metadata.content if metadata else ""
 
     @property
     def charset(self) -> str:
+        """Get the character set used in the book.
+
+        Returns:
+            str: the charset.
+        """
         metadata = self.get_metadata("ncc:charset")
         return metadata.content if metadata else ""
 
     def get_metadata(self, name: str) -> Union[MetaData | None]:
-        metadata = [_ for _ in self._metadata if _.name == name]
-        if len(metadata) == 1:
-            return metadata[0]
-        return None
+        """Get metadat by name.
+
+        Note:
+        - The full name must be specified, like 'dc:langage' (not simply 'langage').
+
+        Args:
+            name (str): the full name (prefix:name).
+
+        Returns:
+            Union[MetaData | None]: the MetaData item (or None).
+        """
+        try:
+            return [_ for _ in self._metadata if _.name == name][0]
+        except IndexError:
+            logger.debug(f"Metadata with name '{name}' not found in the metadata list.")
+            return None
 
     def _populate_entries(self, ncc_document: Document):
         """Process and store the NCC entries (h1 ... h6 tags)."""
